bbc13b06c29f4c816cf115cacbde12ed289d4aff
mbolleng2303 <93711211+mbolleng2303@users.noreply.github.com>
2021-12-10 16:27:54 +0100

    Add files via upload

--- WPO1/calibration.py
+++ WPO1/calibration.py
@@ -3,12 +3,13 @@ import cv2
 import numpy as np
 import os
 import glob
-
-#global variable
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+#  Global variable
 pts = []
 
 
-def get_path(name, output=False):
+def get_path(name, output=False, overwritte = False):
     parent_dir = os.getcwd()
     if not output:
         for file in glob.glob(os.path.join(parent_dir, "Input\\*")):
@@ -20,14 +21,15 @@ def get_path(name, output=False):
         path = os.path.join(parent_dir, 'Output')
         file_dir = os.path.join(path, name)
         if os.path.isfile(file_dir):
-            os.remove(file_dir)
+            if overwritte:
+                os.remove(file_dir)
             return file_dir
         else:
             return file_dir
 
 
-def get_points(text_file):
-    file_path = get_path(text_file)
+def get_points(text_file, output=False):
+    file_path = get_path(text_file, output)
     with open(file_path, "r") as file:
         lines = file.readlines()
         points = np.loadtxt(lines)
@@ -56,12 +58,67 @@ def store_click(image, file_name):
     # close the window
 
     pts = np.array(pts)
-    np.savetxt(get_path(file_name, output=True), pts)
+    np.savetxt(get_path(file_name, output=True, overwritte=True), pts)
 
 
+def compute_m(a, x):
+    u = x[:, 0]
+    v = x[:, 1]
+    m = np.zeros([2*x.shape[0], a.shape[0]])
+    for i in range(x.shape[0]):
+        m[2*i, :] = [a[i, 0], a[i, 1], a[i, 2], 1, 0, 0, 0, 0, -u[i]*a[i, 0], -u[i]*a[i, 1], -u[i]*a[i, 2], -u[i]]
+        m[2*i+1, :] = [0, 0, 0, 0, a[i, 0], a[i, 1], a[i, 2], 1, -v[i]*a[i, 0], -v[i]*a[i, 1], -v[i]*a[i, 2], -v[i]]
+    m = make_svd_decomposition(m)
+    return m
 
 
-get_points("calibration_points3.txt")
+def make_svd_decomposition(m):
+    u, s, v = np.linalg.svd(m)
+    v = v[-1, :]
+    v = np.reshape(np.array(v), (3, 4))
+    v = v/v[-1, -1]
+    return v
 
-store_click(get_path("left.jpg"), "left.txt")
-store_click(get_path("right.jpg"), "right.txt")
+
+def get_2D_from_3D(m, axis):
+    axis = np.array(axis).T
+    proj = np.matmul(m, axis)
+    homogenous = proj[2]
+    x = proj[0]/homogenous
+    y = proj[1]/homogenous
+    return [x, y]
+
+
+def plot (image, pts, origin, axes):
+    im = img.imread(image)
+    plt.imshow(im)
+    pts = np.array(pts)
+    axes = np.array(axes)
+    plt.scatter(x=pts[:, 0], y=pts[:, 1])
+    axe_x = np.array([origin[0], axes[0][0], origin[0], axes[1][0], origin[0], axes[2][0]])
+    axe_y = np.array([origin[1], axes[0][1], origin[1], axes[1][1], origin[1], axes[2][1]])
+    plt.plot(axe_x, axe_y)
+    plt.show()
+
+
+def main():
+    a = get_points("calibration_points3.txt")
+    #store_click(get_path("right.jpg"), "right.txt")
+    x = get_points("right.txt", output=True)
+    m = compute_m(a, x)
+    # re-find axes and points
+    origin = get_2D_from_3D(m, [0, 0, 0, 1])
+    axe_x = get_2D_from_3D(m, [200, 0, 0, 1])
+    axe_y = get_2D_from_3D(m, [0, 200,  0, 1])
+    axe_z = get_2D_from_3D(m, [0, 0, 200, 1])
+    pts_refined = []
+    for i in range(a.shape[0]):
+        pts_refined.append(get_2D_from_3D(m, [a[i, 0], a[i, 1], a[i, 2], 1]))
+    np.savetxt(get_path("right_reconstruction.txt", output=True, overwritte=True), pts_refined)
+    image = get_path("right.jpg")
+    plot(image, pts_refined, origin, [axe_x, axe_y, axe_z])
+
+
+main()
+
+
